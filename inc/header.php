4563cf8b8fc8dc74594475e2d1e7249a09a9b8a8
José Ygor <joseygor@planobweb.com.br>
2016-04-14 17:19:18 -0300

    Iniciando projeto

---
+++ inc/header.php
@@ -0,0 +1,27 @@
+<header>
+    <nav class="navbar navbar-default">
+        <div class="bg-menu"></div>
+        <div class="container-fluid bg-menu-mob">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand visible-xs" href="#home"><img class="img-logo" src="img/Photo-Like-Santos.png" alt="Photo Like Santos"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="#sobre-nos" class="link-menu-top">sobre nós</a></li>
+                    <li><a href="#produtos" class="link-menu-top">produtos</a></li>
+                    <li><a href="#diferenciais" class="link-menu-top">diferenciais</a></li>
+                    <li><a href="#home" class="pd-none hidden-xs"><img class="img-logo" src="img/Photo-Like-Santos.png" alt="Photo Like Santos"></a></li>
+                    <li><a href="#clientes" class="link-menu-top">clientes</a></li>
+                    <li><a href="#galeria" class="link-menu-top">galeria</a></li>
+                    <li><a href="#orcamento" class="link-menu-top">orçamento</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,128 @@
+<?php
+    define(URL, "http://planobweb/clientes/PhotoLovers/Web/");
+?>
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="index, follow">
+    <meta name="description" content="" />
+    <link href="img/icon-tab.png" rel="icon">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <title>Photo Like Santos - Cabines de Fotos</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container-fluid pd-none">
+        <?php include 'inc/header.php'; ?>
+        
+        <!-- Efeito de página individual -->
+        <section id="" class="container-default img-fundo-home bgParallax bgParallax-home" data-speed="15">
+            <!-- <img src="img/inove-o-seu-evento.jpg" alt="" class="img-fundo img-fundo-home bgParallax" data-speed="15"> -->
+            <div class="bloco-conteudo-abs">
+                <div class="bloco-conteudo">
+                    <!-- Página aqui -->
+                    <div class="bloco-titulo-principal bloco-titulo-principal-home">
+                        <img class="pull-left mold-lf-tp" src="img/moldura-lf-tp.png" alt="INOVE O SEU EVENTO">
+                        <img class="pull-right mold-rgt-tp" src="img/moldura-rgt-tp.png" alt="INOVE O SEU EVENTO">
+                        <h1 class="titulo-principal titulo-principal-home pull-left">INOVE O SEU EVENTO</h1>
+                        <img class="pull-right mold-rgt-bt" src="img/moldura-rgt-bt.png" alt="INOVE O SEU EVENTO">
+                        <img class="pull-left mold-lf-bt" src="img/moldura-lf-bt.png" alt="INOVE O SEU EVENTO">
+                    </div>
+                    <!-- FIm Página aqui -->
+                </div>
+            </div>
+        </section>
+        <!-- Fim efeito de página individual -->
+        
+
+         <!-- Efeito de página individual -->
+        <section id="" class="container-default img-fundo-sobre-nos bgParallax" data-speed="15">
+            <!-- <img src="img/sobre-nos.jpg" alt="" class="img-fundo hidden-xs bgParallax" data-speed="15"> -->
+            <div class="bloco-conteudo-abs">
+                <div class="bloco-conteudo">
+                    <!-- Página aqui -->
+                    <div class="bloco-titulo-principal bloco-titulo-principal-sobre">
+                        <img class="pull-left mold-lf-tp" src="img/moldura-sobre-nos-lf-tp.png" alt="INOVE O SEU EVENTO">
+                        <img class="pull-right mold-rgt-tp" src="img/moldura-sobre-nos-rgt-tp.png" alt="INOVE O SEU EVENTO">
+                        <h1 class="titulo-principal titulo-principal-sobre pull-left">sobre nós</h1>
+                        <img class="pull-right mold-rgt-bt" src="img/moldura-sobre-nos-rgt-bt.png" alt="INOVE O SEU EVENTO">
+                        <img class="pull-left mold-lf-bt" src="img/moldura-sobre-nos-lf-bt.png" alt="INOVE O SEU EVENTO">
+                    </div>
+                    <div class="row mg-none-mob">
+                        <br><br>
+                        <div class="bloco-sobre-nos-mob">
+                            <p class="txt-sobre-nos">
+                                A <strong>Photolike Santos</strong> cresceu. Diante da enorme procura em serviços diferenciados, resolvemos expandir e criamos a <strong>PhotoLovers</strong>. Uma empresa nova, cheio de serviços no ramo da fotografia e do entretenimento. Você poderá escolher o serviço que mais lhe agrada.
+                            </p>
+                            <p class="frase-sobre-nos">“Muito mais do que eternizar o momento mais importante da sua vida, nosso objetivo é deixar o seu evento com a sua cara!”</p>
+                            <p class="txt-sobre-nos">
+                                A PhotoLovers é uma empresa especializada em fotografia instantânea de alta qualidade, tiradas e entregues na hora. Nosso objetivo é inovar seu evento, surpreendendo os convidados com um momento divertido e uma foto lembrança personalizada. Sua festa ficará presente na memória e no cotidiano das pessoas. 
+                                Nossos serviços podem ser utilizados em qualquer tipo de evento onde o cliente busque algo inovador que alie entretenimento, diversão e uma recordação eterna para o dono da festa e convidados. Reveladas instantaneamente, as fotos funcionam como uma lembrança criativa, agradando convidados de todas as idades.
+                            </p>
+                        </div>
+                        <div class="row mg-none-mob">
+                            <div class="icones-sobre-nos"><img src="img/icone-casamentos.png" alt=""></div>
+                            <div class="icones-sobre-nos"><img src="img/icone-flores.png" alt=""></div>
+                            <div class="icones-sobre-nos"><img src="img/icone-aniversarios.png" alt=""></div>
+                            <div class="icones-sobre-nos"><img src="img/icone-bolo.png" alt=""></div>
+                            <div class="icones-sobre-nos"><img src="img/icone-formaturas.png" alt=""></div>
+                            <div class="icones-sobre-nos"><img src="img/icone-diploma.png" alt=""></div>
+                            <div class="icones-sobre-nos pd-none"><img src="img/icone-festas-de-final-de-ano.png" alt=""></div>
+                            <div class="icones-sobre-nos"><img src="img/icone-eventos-presenciais.png" alt=""></div>
+                            <div class="icones-sobre-nos"><img src="img/icone-agenda.png" alt=""></div>
+                            <div class="icones-sobre-nos"><img src="img/icone-congressos.png" alt=""></div>
+                            <div class="icones-sobre-nos"><img src="img/icone-festas.png" alt=""></div>
+                            <div class="icones-sobre-nos"><img src="img/icone-acoes-promocionais.png" alt=""></div>
+                            <div class="icones-sobre-nos"><img src="img/icone-fantasia.png" alt=""></div>
+                            <div class="icones-sobre-nos pd-none"><img src="img/icone-outras-ocasioes.png" alt=""></div>
+                        </div>
+                    </div>
+                    <!-- FIm Página aqui -->
+                </div>
+            </div>
+        </section>
+        <!-- Fim efeito de página individual -->
+
+
+        <!-- Efeito de página individual -->
+        <!-- <section id="" class="container-default">
+            <img src="img/inove-o-seu-evento.jpg" alt="" class="img-fundo">
+            <div class="bloco-conteudo-abs">
+                <div class="bloco-conteudo">
+                    Página aqui
+                    FIm Página aqui
+                </div>
+            </div>
+        </section> -->
+        <!-- Fim efeito de página individual -->
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/wow-funcao.js"></script>
+    <script>
+        $('.bgParallax').each(function(){
+            var $obj = $(this);
+            $(window).scroll(function() {
+                var yPos = -($(window).scrollTop() / $obj.data('speed')); 
+                var bgpos = '50% '+ yPos + 'px';
+                // $obj.css('background-position', bgpos );
+                $('.bgParallax-home').css('background-position', bgpos );
+            }); 
+        });
+    </script>
+</body>
+</html>
